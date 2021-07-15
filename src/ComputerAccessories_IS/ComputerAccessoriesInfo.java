e9dabae0ec17a098e615d04317b76cfa595a77b8
dipesh-shrestha00 <70954565+dipesh-shrestha00@users.noreply.github.com>
2021-07-15 12:42:18 +0545

    Add files via upload

---
+++ src/ComputerAccessories_IS/ComputerAccessoriesInfo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComputerAccessories_IS;
+
+/**
+ *
+ * @author DIPESH
+ */
+public class ComputerAccessoriesInfo {
+    public static void main(String args[])
+    {
+        SplashScreen Screen = new SplashScreen();
+        Screen.main(new String[0]);
+    }
+}
